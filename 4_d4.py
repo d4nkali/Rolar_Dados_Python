84db2c98cb3cb52a4b3c96246c748efa1670f769
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-10-27 13:51:21 -0300

    4 d6

---
+++ 4_d4.py
@@ -0,0 +1,13 @@
+# 4 Dados de 6 faces (d6)
+
+from random import randint
+
+print('Os resultados dos dados são: ')
+print()
+
+print(randint(1, 6))
+print(randint(1, 6))
+print(randint(1, 6))
+print(randint(1, 6))
+
+print()
